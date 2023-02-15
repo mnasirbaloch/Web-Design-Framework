e88ac6b9196912c0389d1eac78bb98504c26c4e0
= <->
2023-02-15 14:41:38 +0500

    add missing code

--- python_intro/06_loops_in_python.py
+++ python_intro/06_loops_in_python.py
@@ -1,1 +1,33 @@
+# For loop over a range
+for i in range(5):
+    print(i)
+
+# For loop over a list
+my_list = [1, 2, 3, 4, 5]
+for i in my_list:
+    print(i)
+
+# While loop
+i = 0
+while i < 5:
+    print(i)
+    i += 1
+
+# Loop control statements
+for i in range(10):
+    if i == 3:
+        continue
+    if i == 8:
+        break
+    print(i)
+
+# Looping through dictionaries
+my_dict = {"apple": 1, "banana": 2, "cherry": 3}
+for key, value in my_dict.items():
+    print(key, value)
+
+# Looping through strings
+my_string = "hello world"
+for char in my_string:
+    print(char)
  
