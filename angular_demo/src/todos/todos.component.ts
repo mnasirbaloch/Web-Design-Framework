5965a45c895b86d0aea9b1d94a4bb49a9cfce38f
= <->
2023-02-19 23:49:22 +0500

    adding delete option in todo app

--- angular_demo/src/todos/todos.component.ts
+++ angular_demo/src/todos/todos.component.ts
@@ -7,26 +7,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TodosComponent implements OnInit {
   todo:string  = '';
-
-  todos:any = [];
+  todos:Array<TodoItem> = [];
 
   constructor() {}
-
   ngOnInit() {}
 
   addTodo() {
     if(this.todo.length==0){return;}else{
-    const newTodo = { todo: this.todo, status: 'active' };
+    const newTodo =new TodoItem(this.todo,'active');
     this.todos.push(newTodo);
     this.todo = '';
   }
   }
-
   markAsComplete(todo: { status: string; }) {
     todo.status = 'completed';
   }
 
   undo(todo: { status: string; }) {
     todo.status = 'active';
+  }
+  delete(item:TodoItem) {
+    const index = this.todos.indexOf(item);
+    if (index > -1) {
+      this.todos= this.todos.slice(0, index).concat(this.todos.slice(index + 1));
+
+    }
+   console.log(this.todos.length)
+  }
+
+}
+export class TodoItem{
+  name: string;
+  status:string;
+  constructor(name:string,status:string) {
+    this.name=name;
+    this.status=status;
   }
 }
